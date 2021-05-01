fa040e2cd190028bcd09d0e056e96bbf8305894e
Meet Patel <meetaiims@yahoo.com>
2021-05-01 08:34:28 -0500

    add class ExceptionThrown to demo call stack and exception handling

---
+++ src/com/nautilus1/ExceptionThrown.java
@@ -0,0 +1,48 @@
+package com.nautilus1;
+
+import java.io.IOException;
+
+// Java program to demonstrate how exception is thrown
+// how the runtime system searches the call stack
+public class ExceptionThrown {
+	
+	// It throws the Exception(Arithmetic Exception)
+	// Appropriate Exception handler is not found within this method.
+	static int divideByZero(int a, int b) {
+		
+		// this statement will cause ArithmeticException (/ by zero)
+		int i = a / b;
+		
+		return i;		
+	}
+	
+	// The runtime system searches the appropriate Exception handler
+	// in this method also but couldn't find it. So continuing with the call stack
+	static int computeDivision(int a, int b) {
+		int res = 0;
+		
+		try {
+			res = divideByZero(a, b);
+		}
+		// doesn't match with ArithmeticException
+		catch (NumberFormatException ex) {
+			System.out.println("NumberFormatException has occured");
+		}
+		return res;	
+	}
+	
+	// In this method found appropriate Exception handler
+	// i.e. matching catch block
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 0;
+		try {
+			int i = computeDivision(a, b);
+			System.out.println(i);
+		}
+		catch (ArithmeticException e) {
+			System.out.println(e);
+		}	
+			
+	}
+}
--- src/com/nautilus1/Main.java
+++ src/com/nautilus1/Main.java
@@ -1,11 +1,11 @@
 package com.nautilus1;
+import java.io.*;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello Nautilus1");
-		Area rectangle = new Area(3, 5);
-		int area = rectangle.getArea();
-		System.out.println(area);
-		
+	
+	
+	public static void main(String[] args)  {
+		System.out.println("Hello Main...");
 	}
 }
