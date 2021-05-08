ef98647f21804cf4e96fc521c93296b38cc3faf2
Meet Patel <meetaiims@yahoo.com>
2021-05-08 11:10:51 -0500

    add static fields for sql connection

---
+++ src/FirstJDBCExample.java
@@ -0,0 +1,8 @@
+import java.sql.*;
+
+public class FirstJDBCExample {
+	static final String DB_URL = "jdbc:mysql://localhost/TUTORIALSPOINT";
+	static final String USER = "guest";
+	static final String PASS = "Brotherchoksi12#";
+	static final String QUERY = "SELECT id, first, last, age FROM Employees";
+}
