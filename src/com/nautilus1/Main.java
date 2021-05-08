9a8f698c7f2b933f4948de384d51fd57f94037e0
Meet Patel <meetaiims@yahoo.com>
2021-05-08 10:40:52 -0500

    create new HashMap instance

--- src/com/nautilus1/Main.java
+++ src/com/nautilus1/Main.java
@@ -1,11 +1,18 @@
 package com.nautilus1;
 
 import java.io.*;
+import java.util.*;
 
 public class Main {
 	
-	public static void main(String[] args) throws IOException  {
+	
+	public static void main(String[] args)  {
 		System.out.println("Hello Main...");
-		 	
+		Map<String, Integer> random = new HashMap<>();
+		random.put("RandomOne", 1);
+		random.put("RandomTwo", 1);
+		random.put("RandomThree", 3);
+		random.put("RandomThree", 4567);
+		System.out.println(random);
 	}
 }
