cf55908bb9c5f3e36e23e5fbb1fa0217a5c6100a
Meet Patel <meetaiims@yahoo.com>
2021-06-01 04:33:39 -0400

    add buildHashMapFromCSV() to CSVFileHandler class

--- src/com/nautilus/csvfilehandling/CSVFileHandler.java
+++ src/com/nautilus/csvfilehandling/CSVFileHandler.java
@@ -3,6 +3,7 @@ package com.nautilus.csvfilehandling;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 import com.opencsv.CSVReader;
@@ -23,7 +24,7 @@ public class CSVFileHandler {
 
 			while (nextRecord != null) {
 				for (String cell : nextRecord) {
-					System.out.println(cell + "\t");
+					System.out.print(cell + "\t");
 				}
 				System.out.println();
 				nextRecord = csvReader.readNext();
@@ -48,7 +49,7 @@ public class CSVFileHandler {
 
 			for (String[] row : allData) {
 				for (String cell : row) {
-					System.out.println(cell + "\t");
+					System.out.print(cell + "\t");
 				}
 				System.out.println();
 			}
@@ -57,12 +58,66 @@ public class CSVFileHandler {
 			e.printStackTrace();
 		}
 	}
+	
+	public static HashMap<String,String> buildHashMapFromCSV(String key, String value, String filePath) throws IOException {
+		FileReader fileReader = null;
+		CSVReader csvReader = null;
+		HashMap<String, String> csvHashMap = new HashMap<>();
+		
+		try {
+			fileReader = new FileReader(filePath);
+			
+			csvReader = new CSVReader(fileReader);
+			
+			String[] csvHeaders = csvReader.readNext();
+			
+			int keyIndex = -1;
+			int valueIndex = -1;
+			
+			for (int i = 0; i < csvHeaders.length; i++) {
+				if (csvHeaders[i].equals(key)) {
+					keyIndex = i;
+				} 
+				if (csvHeaders[i].equals(value)) {
+					valueIndex = i;
+				}
+			}
+			
+			System.out.println(keyIndex);
+			System.out.println(valueIndex);
+			
+			if (keyIndex == -1 || valueIndex == -1) {
+				throw new RuntimeException("Key-Value should correspond to valid headers in csv file");
+			}
+			
+			
+			
+			
+			String[] nextRow = csvReader.readNext();
+			
+			while (nextRow != null) {
+				System.out.println(nextRow[keyIndex]);
+				System.out.println(nextRow[valueIndex]);
+				csvHashMap.put(nextRow[keyIndex], nextRow[valueIndex]);
+				nextRow = csvReader.readNext();
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} 
+		finally {
+			csvReader.close();
+		}
+		return csvHashMap;
+		
+	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Hello CSVFileHandler!");
 
-		CSVFileHandler.readAllDataAtOnce(
+		CSVFileHandler.readDataLineByLine(
 				"/home/nautilusshell/eclipse-workspace/April12Training/SeleniumDemoQa/target/test-classes/basses.csv");
-
+		HashMap<String, String> makeByModelHashMap = CSVFileHandler.buildHashMapFromCSV("Model", "Make", "/home/nautilusshell/eclipse-workspace/April12Training/SeleniumDemoQa/target/test-classes/basses.csv");
+		System.out.println(makeByModelHashMap);
 	}
 }
